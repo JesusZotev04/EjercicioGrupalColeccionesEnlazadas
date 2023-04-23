b324778f9bea350ed4df2fb721094787ca61123d
BenjaminBraceli <117902303+BenjaminBraceli@users.noreply.github.com>
2023-04-23 12:47:45 +0200

    Añadida parte Benjamin - Borrar + ToString

---
+++ Codigo/Borrar+ToString.cs
@@ -0,0 +1,21 @@
+/*
+public void Borra(Nodo<T> nodo) {
+    nodo.Anterior.Siguiente = nodo.Siguiente;
+    nodo.Siguiente.Anterior = nodo.Anterior;
+    nodo.Dispose();
+    Longitud--;
+}
+
+public override string ToString() {
+    string salida = "";
+    for (Nodo<T> i = Primero; i != null; i = i.Siguiente)
+        salida += $"[{i.Dato}] ";
+
+    salida += $"-";
+    
+    for (Nodo<T> i = Ultimo; i != null; i = i.Anterior)
+       salida += $"[{i.Dato}]";
+
+    return salida;
+}
+*/
