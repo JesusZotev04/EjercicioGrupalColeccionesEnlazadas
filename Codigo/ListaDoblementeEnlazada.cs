0541130e3b2946c4484513075276e833e9bd06c6
DavidESB <118671109+DavidESB@users.noreply.github.com>
2023-04-23 13:26:38 +0200

    Update ListaDoblementeEnlazada.cs

--- Codigo/ListaDoblementeEnlazada.cs
+++ Codigo/ListaDoblementeEnlazada.cs
@@ -84,7 +84,56 @@ class ListaDoblementeEnlazada<T> : IDisposable, IEnumerable<T> where T : ICompar
 
     // DAVID 
     // public NodoListaDoblementeEnlazada<T> Busca(T dato)
+    public Nodo<T> Busca(T dato)
+{
+    Nodo<T> actual = Primero;
+    while (actual != null)
+    {
+        if (actual.dato.Equals(dato))
+        {
+            return actual;
+        }
+        actual = actual.Siguiente;
+    }
+    return null;
+}
     // public void EditaNodo(T datoAnterior, T datoNuevo, string dirección)
+     public void EditaNodo(T datoAnterior, T datoNuevo, string direccion)
+{
+    if (Primero == null)
+    {
+        Console.WriteLine("La lista está vacía");
+    }
+    Nodo<T> actual;
+    if (direccion.ToLower() == "primero")
+    {
+        actual = Primero;
+        while (actual != null)
+        {
+            if (actual.dato.Equals(datoAnterior))
+            {
+                actual.dato = datoNuevo;
+            }
+            actual = actual.Siguiente;
+        }
+    }
+    else if (direccion.ToLower() == "ultimo")
+    {
+        actual = Ultimo;
+        while (actual != null)
+        {
+            if (actual.dato.Equals(datoAnterior))
+            {
+                actual.dato = datoNuevo;
+            }
+            actual = actual.Anterior;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Dirección inválida");
+    }
+}
 
     public IEnumerator<T> GetEnumerator() => new Enumerador(Primero);
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
